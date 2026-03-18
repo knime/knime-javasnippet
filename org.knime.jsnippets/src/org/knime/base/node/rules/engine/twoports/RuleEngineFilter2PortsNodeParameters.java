a04fe27163674b03b8c9b1b8e4b6e250588b49de
Jochen Reißinger <jochen.reissinger@tngtech.com>
2026-03-18 14:16:16 +0100

    UIEXT-3470: WebUI migration of three Rule nodes (Dictionary)
    * Rule Engine Variable (Dictionary)
    * Rule-based Row Splitter (Dictionary)
    * Rule-based Row Filter (Dictionary)
    
    UIEXT-3470 (WebUI Migration of three Rule nodes (Dictionary))

---
+++ org.knime.jsnippets/src/org/knime/base/node/rules/engine/twoports/RuleEngineFilter2PortsNodeParameters.java
@@ -0,0 +1,71 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Mar 19, 2026 (Jochen Reißinger, TNG Technology Consulting GmbH): created
+ */
+package org.knime.base.node.rules.engine.twoports;
+
+import org.knime.node.parameters.Widget;
+import org.knime.node.parameters.persistence.Persist;
+
+/**
+ * Node parameters for the Rule-based Row Filter (Dictionary) node.
+ * <p>
+ * Extends {@link AbstractRuleEngine2PortsNodeParameters} with the filter-specific "Include TRUE matches" checkbox.
+ *
+ * @author Jochen Reißinger, TNG Technology Consulting GmbH
+ * @since 5.12
+ */
+@SuppressWarnings("restriction")
+final class RuleEngineFilter2PortsNodeParameters extends AbstractRuleEngine2PortsNodeParameters {
+
+    @Widget(title = "Include TRUE matches",
+        description = "When checked, rows where the first matching rule evaluates to TRUE are included in the output. "
+            + "When unchecked, those rows are excluded. Rows where no rule matches, or the first matching rule "
+            + "evaluates to FALSE, receive the opposite treatment.")
+    @Persist(configKey = RuleEngineFilter2PortsNodeModel.CFGKEY_INCLUDE_ON_MATCH)
+    boolean m_includeOnMatch = RuleEngineFilter2PortsNodeModel.DEFAULT_INCLUDE_ON_MATCH;
+}
